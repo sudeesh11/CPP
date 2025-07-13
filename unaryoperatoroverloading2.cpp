d9d798d3dba8e8a22f18f7703af2c7cb711da198
Sudeesh <sudeesh7acharya@gmail.com>
2025-07-14 00:40:04 +0545

    Unary Operator Overloading

--- unaryoperatoroverloading2.cpp
+++ unaryoperatoroverloading2.cpp
@@ -27,23 +27,25 @@ public:
     {
         cout << real << "+" << imag << "i" << endl;
     }
-
-    ComplexNum operator++() // prefix
-    {
-        ComplexNum temp;
-        temp.real = ++real;
-        temp.imag = ++imag;
-        return temp;
-    }
-
-    ComplexNum operator++(int) // postfix
-    {
-        ComplexNum temp;
-        temp.real = real++;
-        temp.imag = imag++;
-        return temp;
-    }
+    friend ComplexNum operator++(ComplexNum &);
+    friend ComplexNum operator++(ComplexNum &, int);
 };
+
+ComplexNum operator++(ComplexNum &c1) // prefix
+{
+    ComplexNum temp;
+    temp.real = ++c1.real;
+    temp.imag = ++c1.imag;
+    return temp;
+}
+
+ComplexNum operator++(ComplexNum &c2, int) // postfix
+{
+    ComplexNum temp;
+    temp.real = c2.real++;
+    temp.imag = c2.imag++;
+    return temp;
+}
 
 int main()
 {
